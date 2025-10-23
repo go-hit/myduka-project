66376b99b8771774bc4f342d0c7f1f538d167a65
mg <godymg71@gmail.com>
2025-10-23 21:06:07 +0300

    intro to flask

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+# home route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# products route
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+# sales route
+@app.route('/sales')
+def sales():
+    return render_template('sales.html')
+
+# stock route
+@app.route('/stock')
+def stock():
+    return render_template('stock.html')
+
+app.run()
